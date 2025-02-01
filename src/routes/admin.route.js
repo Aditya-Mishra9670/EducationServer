4ae801de9eeafc3986d24f2d9a2431640042ec68
Aditya-Mishra9670 <codingbyaditya2005@gmail.com>
2025-02-01 09:54:12 +0530

    ADDED routes of admin

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {allUsers, getUserbyId,createNotification, deleteUserbyId} from "../controllers/admin.controller.js";
+import {allUsers, getUserbyId,createNotification, deleteUserbyId, getReports, getReportedContent, reviewReport} from "../controllers/admin.controller.js";
 const router = express.Router();
 
 
@@ -8,5 +8,8 @@ router.get('/allUsers',allUsers);
 router.get('/:userId',getUserbyId);
 router.post('/createNotification',createNotification);
 router.delete('/removeUser',deleteUserbyId);
+router.get('/reports',getReports);
+router.get('/specificReport',getReportedContent);
+router.post('reviewReport',reviewReport);
 
 export default router;
